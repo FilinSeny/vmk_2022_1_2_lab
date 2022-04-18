272e1f6ead37638df75f22ba927b5bde5d368397
FilinSeny <arsen.domoded@gmail.com>
2022-04-18 22:43:55 +0300

    Only root and simpson

---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <math.h>
+
+///f1 = 3 (0.5 / (x + 1) + 1)   f' = 1.5/(x+1)^2
+///f2 = 2.5x − 9.5              f' = 2.5
+///f3 = 5 / x                   f' = 5/x^2
+///get_val_f1.asm
+///get_val_f2.asm
+///get_val_f3.asm
+
+
+double get_val(const double x, const int num_func) {
+    return 1;
+}
+
+
+double get_first_p(const double x, const int num_func) {
+    return 1;
+}
+
+
+double get_root(double (*f)(double), double (*fd)(double), double (*g)(double), double (*gd)(double), double a, double b, const double e, int* iteratinon) {
+    double F_a = f(a) - g(a);
+    double F_b = f(b) - g(b);
+    iteratinon = 0;
+
+
+    while (fabs(a - b) > e) {
+        (*iteratinon)++;
+        int first_p = 1;
+        if (F_a > 0) {
+            first_p = -1;
+        }
+        int second_p = 1;
+        double mid = (a + b) / 2;
+        double F_mid = f(mid) - g(mid);
+        if (F_mid > ((F_a + F_b) / 2)) {
+            second_p = -1;
+        }
+
+        double cross_point;
+        if (first_p * second_p > 0) {
+            cross_point = b - F_b / (fd(b) - gd(b));
+            b = cross_point;
+            F_b = f(cross_point) - g(cross_point);
+        } else {
+            cross_point = F_a / (fd(a) - gd(a));
+            a = cross_point;
+            F_a = f(cross_point) - g(cross_point);
+        }
+    }
+
+    return a;
+}
+
+
+double get_integral(double (*f)(double), double (*g)(double), const double a, const double b, int * iterations) {
+    int n= 10;
+    *iterations = 0;
+    double sum_0 = 0;
+    double sum_1 = 0;
+    double F_0 = f(a) - g(a);
+    double F_n = f(b) - g(b);
+    double h = (b - a) / n;
+    for (int i = 1; i < 10; ++i) {
+        if (i % 2 == 0) {
+            sum_0 += f(a + i*h) - g(a + i*h);
+        } else {
+            sum_1 += f(a + i*h) - g(a + i*h);
+        }
+    }
+
+    double I_n;
+    double I_2n = h/3 * (F_0 + 4 * sum_1 + 2 * sum_0 + F_n);
+    do {
+        (*iterations)++;
+        I_n = I_2n;
+        sum_0 += sum_1;
+        n *= 2;
+        h = (b - a) / n;
+        sum_1 = 0;
+        for (int i = 1; i < 2*n; i += 2) {
+            sum_1 += f(a + i*n) - g(a + i*h);
+        }
+        I_2n = h/3 * (F_0 + 4 * sum_1 + 2 * sum_0 + F_n);
+    } while (1/15 * fabs(I_n - I_2n));
+
+    return I_2n;
+}
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
